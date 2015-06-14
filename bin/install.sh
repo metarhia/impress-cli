09615249cb47d0c54dffd40272465953e025fa89
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2015-06-14 14:12:14 +0300

    Version 0.1.279

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 chmod +x ./bin/uninstall.sh
 chmod +x ./bin/impress
-ln -s ./bin/impress /etc/init.d
+ln -s ./bin/impress /etc/init.d/impress
 if [ -f /etc/debian_version ]; then
   sudo update-rc.d impress defaults
 elif [ $(pidof systemd) -eq 1 ]; then
-  ln -s ./bin/impress.service /etc/systemd/system
+  ln -s ./bin/impress.service /etc/systemd/system/impress.service
   systemctl daemon-reload
   systemctl enable impress
 elif [ -f /etc/redhat-release ]; then
