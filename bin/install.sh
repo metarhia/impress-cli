7d1398be5e6837872e2bb972c680838ff1a3fc00
johnbizokk <johnbizokk@users.noreply.github.com>
2015-08-01 01:40:48 +0300

    Fixed installation script

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,7 @@ chmod +x $(pwd)/bin/impress
 ln -s -f $(pwd)/cli.js /bin/impress
 rm -f /etc/init.d/impress
 ln -f $(pwd)/bin/impress /etc/init.d/impress
-if [ -n "command -v systemctl" ]; then
+if [ -n "$(command -v systemctl)" ]; then
   rm -f /etc/systemd/system/impress.service
   ln -f $(pwd)/bin/impress.service /etc/systemd/system/impress.service
   systemctl daemon-reload
