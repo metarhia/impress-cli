967afad4d9d0a654f75747d4f00063a4dcea57a1
tshemsedinov <timur.shemsedinov@gmail.com>
2018-07-29 00:28:13 +0300

    Minor code style changes
    PR-URL: https://github.com/metarhia/impress-cli/pull/36

--- cli.js
+++ cli.js
@@ -19,9 +19,11 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const nodePar = (
-  '--stack-trace-limit=1000 --allow-natives-syntax --max_old_space_size=2048'
-);
+const nodePar = [
+  ['stack-trace-limit', 1000],
+  ['allow-natives-syntax'],
+  ['max_old_space_size', 2048]
+].map(par => '--' + par.join('=')).join(' ');
 
 const linkFileName = __dirname + '/impress.link';
 const existsLink = fs.existsSync(linkFileName);
@@ -63,7 +65,7 @@ global.applications = [];
 
 // Execute shell command displaying output and possible errors
 const execute = (cmd, callback) => {
-  cp.exec(cmd, { cwd: __dirname }, (error, stdout /* stderr */) => {
+  cp.exec(cmd, { cwd: __dirname }, (error, stdout) => {
     if (error) {
       console.log(concolor.error(error.toString()));
     } else {
@@ -127,7 +129,7 @@ const commands = {
 
     const doInput = () => {
       rl.question('Enter application name: ', (answer) => {
-        if (applications.indexOf(answer) === -1) {
+        if (!applications.includes(answer)) {
           applicationName = answer;
           doAdd();
           doExit();
